0ddf0a31a80e18470f13141675dfd11b8e809f73
Yordano Pascual <yordanoweb@gmail.com>
2025-01-11 22:00:34 -0500

    save and load assets from local storage

--- js/balance.js
+++ js/balance.js
@@ -1,1 +1,46 @@
-console.log("Balance calculator");
+;(function balanceMainFunction() {
+
+  // Parent form element
+  const formEl = document.getElementById("balance-form");
+
+  function getAssetInputs() {
+    const inputs = [];
+
+    // Get all div elements with class 'row' within the parent form
+    const inputElements = formEl.querySelectorAll('input.asset-input');
+
+    // Loop through each row
+    inputElements.forEach(input => {
+      inputs.push(input);
+    });
+
+    return inputs;
+  }
+
+  function updateAssets() {
+    const inputs = getAssetInputs();
+    const values = [];
+    inputs.forEach(input => {
+      values.push(input.value);
+    });
+    localStorage.setItem("assets", JSON.stringify(values));
+  }
+
+  ///////////////////////////////////////////////////
+  // THE ACTION MOVIE STARTS HERE...
+  ///////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////
+  // Get assets from local storage
+  ///////////////////////////////////////////////////
+  const storedAssets = JSON.parse(localStorage.getItem("assets"));
+
+  if (storedAssets) {
+    // Loop through each input and set retrieved asset from storage
+    getAssetInputs(formEl).forEach((input, i) => {
+      input.value = storedAssets[i];
+      input.addEventListener('input', updateAssets);
+    });
+  }
+
+})()
